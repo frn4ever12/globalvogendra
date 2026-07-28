48183f3f5094e2ba8a5b59afa2ac6ab018354f6c
sanjay dahal <sanjay.dahal90@gmail.com>
2026-07-28 14:09:11 +0545

    Fix jQuery version compatibility with Bootstrap 3.4.1

--- resources/views/Admin/includes/bottom.blade.php
+++ resources/views/Admin/includes/bottom.blade.php
@@ -1,4 +1,4 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <!-- Bootstrap CDN -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js"></script>
 <!-- FastClick CDN -->
