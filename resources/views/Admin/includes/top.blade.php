7eaa84ec4c82d89b0d1729c874dce40b73e884f3
sanjay dahal <sanjay.dahal90@gmail.com>
2026-07-28 14:22:41 +0545

    Fix CDN URLs and add status column to menus table

--- resources/views/Admin/includes/top.blade.php
+++ resources/views/Admin/includes/top.blade.php
@@ -14,15 +14,15 @@
 <!-- NProgress CDN -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" rel="stylesheet">
 <!-- iCheck CDN -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/flat/green.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/flat/green.css" rel="stylesheet">
 
 <!-- bootstrap-progressbar CDN -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-progressbar@3.3.4/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-progressbar/3.3.4/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
 <!-- JQVMap CDN -->
-<link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet" />
+<link href="https://cdnjs.cloudflare.com/ajax/libs/jqvmap/1.5.1/jqvmap.min.css" rel="stylesheet" />
 <!-- bootstrap-daterangepicker CDN -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-daterangepicker@3.1.0/daterangepicker.min.css" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/malihu-custom-scrollbar-plugin@3.1.5/jquery.mCustomScrollbar.min.css" rel="stylesheet"/>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/3.1.0/daterangepicker.min.css" rel="stylesheet">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/malihu-custom-scrollbar-plugin/3.1.5/jquery.mCustomScrollbar.min.css" rel="stylesheet"/>
 
 <!-- Custom Theme Style - inline for now -->
 <style>
