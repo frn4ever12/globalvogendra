2398173fac9e314b7607f68799ecd070367839c2
sanjay dahal <sanjay.dahal90@gmail.com>
2026-07-29 16:22:30 +0545

    Fix menu slug column error and update views to use modern layout

--- resources/views/Admin/Menu/create.blade.php
+++ resources/views/Admin/Menu/create.blade.php
@@ -1,49 +1,54 @@
-@extends('Admin.includes.main')
+@extends('Admin.includes.modern-main')
 @section('content')
-    <div style="margin-bottom: 1.5rem;">
-        <h3><b>Add a New Menu<span style="color: red; font-size: 1.3rem; "></span></b></h3>
+    <div class="page-header">
+        <h3 class="page-title">Add New Menu</h3>
     </div>
-    <form action="{{ route('admin.menu.store') }}" method="post">
-        @csrf
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 form-group">
-                <label>Name <span style="color:red;">*</span></label>
-                <input class="form-control" placeholder="Menu Name" type="text" name="name" />
-                @error('name')
-                    <span class="error-message">{{ $message }}</span>
-                @enderror
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 form-group">
-                <label>Icon (FontAwesome class)</label>
-                <input class="form-control" placeholder="e.g., fa fa-home" type="text" name="icon" />
-                @error('icon')
-                    <span class="error-message">{{ $message }}</span>
-                @enderror
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 form-group">
-                <label>Order</label>
-                <input class="form-control" placeholder="Order" type="number" name="order_no" value="0" />
-                @error('order_no')
-                    <span class="error-message">{{ $message }}</span>
-                @enderror
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 form-group">
-                <label>Status</label>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="status" id="status" checked>
-                    <label class="form-check-label" for="status">Active</label>
+    
+    <div class="card">
+        <div class="card-body">
+            <form action="{{ route('admin.menu.store') }}" method="post">
+                @csrf
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-12 form-group">
+                        <label>Name <span style="color:red;">*</span></label>
+                        <input class="form-control" placeholder="Menu Name" type="text" name="name" />
+                        @error('name')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 form-group">
+                        <label>Icon (FontAwesome class)</label>
+                        <input class="form-control" placeholder="e.g., fa fa-home" type="text" name="icon" />
+                        @error('icon')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 form-group">
+                        <label>Order</label>
+                        <input class="form-control" placeholder="Order" type="number" name="order_no" value="0" />
+                        @error('order_no')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-12 form-group">
+                        <label>Status</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="status" id="status" checked>
+                            <label class="form-check-label" for="status">Active</label>
+                        </div>
+                        @error('status')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
                 </div>
-                @error('status')
-                    <span class="error-message">{{ $message }}</span>
-                @enderror
-            </div>
-        </div>
 
-        <div class="form-group row">
-            <div class="col-sm-12">
-                <button type="submit" class="btn btn-primary">Save</button>
-                <a href="{{ route('admin.menu.index') }}" class="btn btn-secondary">Cancel</a>
-            </div>
+                <div class="form-group row">
+                    <div class="col-sm-12">
+                        <button type="submit" class="btn btn-primary">Save</button>
+                        <a href="{{ route('admin.menu.index') }}" class="btn btn-secondary">Cancel</a>
+                    </div>
+                </div>
+            </form>
         </div>
-    </form>
+    </div>
 @endsection
--- resources/views/Admin/Menu/index.blade.php
+++ resources/views/Admin/Menu/index.blade.php
@@ -1,57 +1,48 @@
-@extends('Admin.includes.main')
-@section('head')
-    @include('Admin.includes.datatables-css')
-@endsection
+@extends('Admin.includes.modern-main')
 @section('content')
-    <div style="display: flex;justify-content: space-between;align-items: center;flex-wrap: wrap;margin-bottom: 1.5rem;">
-        <h3>
-            Menus
-        </h3>
-        <div>
-            <a href="{{ route('admin.menu.create') }}" class="btn btn-success">
-                <i class="fa fa-plus"></i>&nbsp;
-                <span>Add New</span>
+    <div class="page-header">
+        <div style="display: flex;justify-content: space-between;align-items: center;flex-wrap: wrap;">
+            <h3 class="page-title">Menus</h3>
+            <a href="{{ route('admin.menu.create') }}" class="btn btn-primary">
+                <i class="fa fa-plus"></i>&nbsp; Add New
             </a>
         </div>
     </div>
-    <div>
-        <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0"
-            width="100%">
-            <thead>
-                <tr>
-                    <th>S.N</th>
-                    <th>Name</th>
-                    <th>Slug</th>
-                    <th>Icon</th>
-                    <th>Order</th>
-                    <th>Status</th>
-                    <th>Sub Menus</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                @foreach ($menus as $menu)
+    
+    <div class="card">
+        <div class="card-body">
+            <table class="table table-striped table-bordered">
+                <thead>
                     <tr>
-                        <td>{{ $loop->iteration }}</td>
-                        <td>{{ $menu->name }}</td>
-                        <td>{{ $menu->slug }}</td>
-                        <td>{{ $menu->icon ?? '-' }}</td>
-                        <td>{{ $menu->order_no }}</td>
-                        <td><span class="btn btn-sm {{ $menu->status ? 'btn-success' : 'btn-danger' }}">{{ $menu->status ? 'Active' : 'Inactive' }}</span></td>
-                        <td>{{ $menu->subMenus->count() }}</td>
-                        <td>
-                            <a href="{{ route('admin.menu.edit', $menu->id) }}"
-                                class="btn btn-sm btn-success"><i class="fa fa-pencil"></i></a>
-                            <button type="button" data-route="{{ route('admin.menu.destroy', $menu->id) }}"
-                                class="btn btn-sm btn-danger deleteBtn"><i class="fa fa-trash"></i></button>
-                        </td>
+                        <th>S.N</th>
+                        <th>Name</th>
+                        <th>Icon</th>
+                        <th>Order</th>
+                        <th>Status</th>
+                        <th>Sub Menus</th>
+                        <th>Action</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+
+                <tbody>
+                    @foreach ($menus as $menu)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $menu->name }}</td>
+                            <td>{{ $menu->icon ?? '-' }}</td>
+                            <td>{{ $menu->order_no }}</td>
+                            <td><span class="badge {{ $menu->status ? 'bg-success' : 'bg-danger' }}">{{ $menu->status ? 'Active' : 'Inactive' }}</span></td>
+                            <td>{{ $menu->subMenus->count() }}</td>
+                            <td>
+                                <a href="{{ route('admin.menu.edit', $menu->id) }}"
+                                    class="btn btn-sm btn-success"><i class="fa fa-pencil"></i></a>
+                                <button type="button" data-route="{{ route('admin.menu.destroy', $menu->id) }}"
+                                    class="btn btn-sm btn-danger deleteBtn"><i class="fa fa-trash"></i></button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
-@endsection
-@section('scripts')
-    @include('Admin.includes.datatables-scripts')
 @endsection
